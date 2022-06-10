3a79d380dd51dacdb564dcf1eeb8b3734fa59267
Matthew Pierce <matthew@cybereon.com.au>
2022-06-10 17:00:42 +0800

    Establish boilerplate App class

--- src/index.ts
+++ src/index.ts
@@ -8,47 +8,65 @@
     by CybernetixZero
 */
 
-import { Client, Intents } from 'discord.js';
-//import { REST } from '@discordjs/rest';
-
+import { Client, Intents, Interaction, MessageReaction, PartialMessageReaction, User, PartialUser } from 'discord.js';
+import { REST } from '@discordjs/rest';
 import { ConfigService } from './services/config-service';
 
-const configService = new ConfigService();
-configService.load();
+class App {
+     configService?: ConfigService;
+     client?: Client;
+     rest?: REST;
 
-const client = new Client({
-     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
-     partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER']
-});
+     run() {
+          this.showLogo();
 
-/* const rest = new REST({ version: '9' })
-     .setToken(configService.token); */
+          this.configService = new ConfigService();
+          this.configService.load();
+          console.log('Configuration has been loaded.');
+          console.log(this.configService);
 
-client.once('ready', async () => {
-     printLogo();
-     console.log('ready');
-});
+          this.client = new Client({
+               intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+               partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER']
+          });
+          this.client.once('ready', this.onClientReady);
+          this.client.on('interactionCreate', this.onClientInteractionCreate);
+          this.client.on('messageReactionAdd', this.onClientMessageReactionAdd);
+          this.client.on('messageReactionRemove', this.onClientMessageReactionRemove);
 
-client.on('interactionCreate', async (interaction) => {
-     console.log(interaction);
-});
+          this.rest = new REST({ version: '9' })
+               .setToken(this.configService.token);
 
-client.on('messageReactionAdd', async (reaction, user) => {
-     console.log(reaction);
-     console.log(user);
-});
+          //this.client.login(this.configService.token);
+          //console.log('Client has logged in.');
+     }
 
-client.on('messageReactionRemove', async (reaction, user) => {
-     console.log(reaction);
-     console.log(user);
-});
+     showLogo() {
+          console.log('       .__                         ___.           __   _______________  _______  _______   \n  _____|  |____  _  _______    ____\\_ |__   _____/  |_/   __   \\   _  \\ \\   _  \\ \\   _  \\  \n /  ___/  |  \\ \\/ \\/ /\\__  \\  /    \\| __ \\ /  _ \\   __\\____    /  /_\\  \\/  /_\\  \\/  /_\\  \\ \n \\___ \\|   Y  \\     /  / __ \\|   |  \\ \\_\\ (  <_> )  |    /    /\\  \\_/   \\  \\_/   \\  \\_/   \\\n/____  >___|  /\\/\\_/  (____  /___|  /___  /\\____/|__|   /____/  \\_____  /\\_____  /\\_____  /\n     \\/     \\/             \\/     \\/    \\/                            \\/       \\/       \\/ ');
+          console.log('    by CybernetixZero and n3rf_herter\n');
+     }
 
-//client.login(configService.token);
+     async onClientReady() {
+          console.log('ready');
+     }
 
-const printLogo = () => {
-     console.log('       .__                         ___.           __   _______________  _______  _______   \n  _____|  |____  _  _______    ____\\_ |__   _____/  |_/   __   \\   _  \\ \\   _  \\ \\   _  \\  \n /  ___/  |  \\ \\/ \\/ /\\__  \\  /    \\| __ \\ /  _ \\   __\\____    /  /_\\  \\/  /_\\  \\/  /_\\  \\ \n \\___ \\|   Y  \\     /  / __ \\|   |  \\ \\_\\ (  <_> )  |    /    /\\  \\_/   \\  \\_/   \\  \\_/   \\\n/____  >___|  /\\/\\_/  (____  /___|  /___  /\\____/|__|   /____/  \\_____  /\\_____  /\\_____  /\n     \\/     \\/             \\/     \\/    \\/                            \\/       \\/       \\/ ');
-     console.log('    by CybernetixZero\n');
-};
+     async onClientInteractionCreate(interaction: Interaction) {
+          console.log('interactionCreate');
+          console.log(interaction);
+     }
 
-printLogo();
-console.log(configService);
+     async onClientMessageReactionAdd(reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser) {
+          console.log('messageReactionAdd');
+          console.log(reaction);
+          console.log(user);
+     }
+
+     async onClientMessageReactionRemove(reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser) {
+          console.log('messageReactionRemove');
+          console.log(reaction);
+          console.log(user);
+     }
+}
+
+let app = new App();
+app.run();
